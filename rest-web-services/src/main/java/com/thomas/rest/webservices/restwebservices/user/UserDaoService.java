b0c06c7775ded8bfdf8a36bf55a274ebdb73f3ab
Thomas Yap <yaptathong@gmail.com>
2024-05-22 17:46:28 +0800

    Updating repo structure

--- rest-web-services/src/main/java/com/thomas/rest/webservices/restwebservices/user/UserDaoService.java
+++ rest-web-services/src/main/java/com/thomas/rest/webservices/restwebservices/user/UserDaoService.java
@@ -9,34 +9,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserDaoService {
-	//UserDaoService > Static List
-	//JPA/Hibernate > Database
-	//public List<User> findAll()
-	//public User save(User user){
-	//public User findOne(int id){
-	
+	// UserDaoService > Static List
+	// JPA/Hibernate > Database
+	// public List<User> findAll()
+	// public User save(User user){
+	// public User findOne(int id){
+
 	private static List<User> users = new ArrayList<>();
-	
+
 	static {
 		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
 		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
 		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
 	}
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return users;
 	}
-	
-	public User findOne(int id){
+
+	public User findOne(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
 	}
-//		User userFound = null;
-//		for (int i=0; i<users.size();i++) {
-//			if(i==id-1) {
-//				 userFound = users.get(i);
-//			}
-//		}
-//		return userFound;
-	
+
 }
