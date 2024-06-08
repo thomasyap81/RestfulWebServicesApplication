7e1e983b20e8d0634f879053b66bb7d1feed1143
Thomas Yap <yaptathong@gmail.com>
2024-06-09 00:00:08 +0800

    Implementing Dynamic Filtering for REST API

--- rest-web-services/src/main/java/com/thomas/rest/webservices/restwebservices/filtering/SomeBean.java
+++ rest-web-services/src/main/java/com/thomas/rest/webservices/restwebservices/filtering/SomeBean.java
@@ -1,12 +1,14 @@
 package com.thomas.rest.webservices.restwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //@JsonIgnoreProperties({"field2","field3"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
-	@JsonIgnore
+//	@JsonIgnore
 	private String field2;
 	private String field3;
 
