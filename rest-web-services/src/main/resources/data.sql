12d92fa68e93719c523787774ee1a536128cf193
Thomas Yap <yaptathong@gmail.com>
2024-06-10 23:30:11 +0800

    Creating Post Entity with Many-toOne Relationship with User Entity

--- rest-web-services/src/main/resources/data.sql
+++ rest-web-services/src/main/resources/data.sql
@@ -7,3 +7,14 @@ values (10002, current_date(), 'Thomas');
 insert into user_details(id,Birth_Date,Name)
 values (10003, current_date(), 'Cherry');
 
+insert into post (id,description, user_id)
+values(2001,'I want to learn AWS',10001);
+
+insert into post (id,description, user_id)
+values(2002,'I want to learn DevOps',10001);
+
+insert into post (id,description, user_id)
+values(2003,'I want to get AWS Certified',10002);
+
+insert into post (id,description, user_id)
+values(2004,'I want to learn Multi Cloud',10002);
